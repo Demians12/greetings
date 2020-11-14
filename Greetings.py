063887103d5c7b6af5ed1587f7e350b3f15ebff3
Demians12 <maxsantos_d@hotmail.com>
2020-11-14 16:00:23 -0300

    Init

--- Greetings.py
+++
@@ -1,6 +0,0 @@
-print('------------------------------Welcome To My First Program Stranger---------------------------')
-print("I want to know you, so...")
-name = input("What's your name?")
-print(f"Nice to meet you {name}")
-print("My name is Max, the same name of my creator")
-print(f"Feel free to take a look on my other codes and follow me on Github ;). See you {name}")
